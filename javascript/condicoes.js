02061afab0d29817e411f0e79dce8e6edc0854a9
gi-braga <giselebragailustra@gmail.com>
2025-07-04 06:12:52 -0300

    0612

---
+++ javascript/condicoes.js
@@ -0,0 +1,14 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+of (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga usando sempre cinto de segurança!`)
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
