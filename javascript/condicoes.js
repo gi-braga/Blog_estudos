3a5af902112d6eaa0bc35493b5d6fc409a94e35c
gi-braga <giselebragailustra@gmail.com>
2025-07-04 06:56:03 -0300

    0655

--- javascript/condicoes.js
+++ javascript/condicoes.js
@@ -1,6 +1,6 @@
-var vel = 60.5
+/* var vel = 60.5
 console.log(`A velocidade do seu carro é ${vel} Km/h.`)
-of (vel > 60) {
+if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 console.log(`Diriga usando sempre cinto de segurança!`)
@@ -11,4 +11,15 @@ if (país == 'Brasil') {
     console.log(`Você é brasileiro!`)
 } else {
     console.log(`Você é estrangeiro!`)
-}
+} */
+
+    var idade = 18
+    if (idade < 16) {
+        console.log('Não vota')
+    } else {
+        if (idade < 18) {
+            console.log('Voto opcional')
+        } else {
+            console.log('Voto obrigatório')
+        }
+    }
