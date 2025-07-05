3d95de6ab70bc4f126d4be2b0d0210d9d58d1d7c
gi-braga <giselebragailustra@gmail.com>
2025-07-05 11:56:04 -0300

    1156

---
+++ Jogo_pedraPapelTesoura/script3.js
@@ -0,0 +1,8 @@
+function jogar() {
+    const opcoes = ['pedra', 'papel', 'tesoura']
+    const escolha = document.getElementsByName('escolha')
+    let escolhaUsuario = ''
+    let escolhaComputador = opcoes[Math.floor(Math.random() * 3) + 1]
+
+    
+}
