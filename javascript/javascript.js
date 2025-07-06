5ab2b3059171a7b4ba53374e23c90117819fa4b1
gi-braga <giselebragailustra@gmail.com>
2025-07-06 13:04:02 -0300

    1303

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -140,4 +140,64 @@ if (pos == -1) {
     console.log(`O valor 8 está na posição ${pos}`)
 }
 
+
+function parimpar(n) {
+    if (n%2 == 0 ) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimpar(5)
+console.log(`O número é ${res}`)
+
+
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+let res = soma(7, 2)
+console.log(`A soma é ${res}`)
+
+
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
+
+
+// fatorial de um numero 
+
+function fatorial(n) {
+    let fat = 1
+    for (let c = n; c > 1; c--) {
+        fat *= c // fat = fat * c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
+
+// RECURSIVIDADE
+// 5! = 5 x 4 x 3 x 2 x 1
+// 5! = 5 x 4!
+
+// n! = n x (n-1)!
+1! = 1
+
+
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
+
+
 */
