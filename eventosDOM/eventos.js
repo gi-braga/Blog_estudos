d76eac5ec7edf5f6b053a81436223f9d6c683e78
gi-braga <giselebragailustra@gmail.com>
2025-07-08 09:12:03 -0300

    0912

--- eventosDOM/eventos.js
+++ eventosDOM/eventos.js
@@ -2,6 +2,31 @@ const caixa1 = document.querySelector("#caixa1")
 const caixa2 = document.querySelector("#caixa2")
 const botao1 = document.querySelector("#botao")
 const todosCursos = [...document.querySelectorAll(".curso1")]
+//const btc1 = document.querySelector("#c1")
+
+caixa1.addEventListener("click", (evento)=>{
+    console.log("clicou")
+})
+
+/*
+//STOP PROPAGATION: Impede que outros elementos na div disparem o evento
+btc1.addEventListener("click", (evento)=>{
+    evento.stopPropagation() //parou a propagação do evento
+})
+*/
+
+todosCursos.map((el)=>{
+    el.addEventListener("click",(evento)=>{
+        evento.stopPropagation()
+    })
+})
+
+/*
+//REFERENCIANDO ELEMENTOS NO DOM (Parent, Child, Siblings..)
+console.log(caixa1.children)
+console.log(document.getRootNode)
+*/
+
 
 todosCursos.map((el)=>{
     el.addEventListener("click",(evt)=>{
