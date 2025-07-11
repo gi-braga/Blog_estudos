a448fd9f969b3687d7f82d6dc103b40cbeeadca5
gi-braga <giselebragailustra@gmail.com>
2025-07-11 12:39:23 -0300

    1239

---
+++ calculadora/script.js
@@ -0,0 +1,5 @@
+//receber valores digitados no input
+//variavel para armazenar valor 1
+//variavel para armazenar valor 2
+//funções para realizar operações aritméticas (soma, subt, mult, divi, modu, raiz, del, zero)
+//imprimir na tela o resultado
