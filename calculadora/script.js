dde7b9934b9674e8e0b6e19fedd950349b7592dc
gi-braga <giselebragailustra@gmail.com>
2025-07-11 14:04:22 -0300

    1404

--- calculadora/script.js
+++ calculadora/script.js
@@ -3,3 +3,11 @@
 //variavel para armazenar valor 2
 //funções para realizar operações aritméticas (soma, subt, mult, divi, modu, raiz, del, zero)
 //imprimir na tela o resultado
+
+const numeros = document.querySelectorAll("input")
+const telaResultado = document.querySelector("result")
+let conta
+
+function botao(value) {
+    conta = telaResultado.value += value
+}
