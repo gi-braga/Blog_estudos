e97c1a8a06d0be748e4b982e7cb188ae616274c3
gi-braga <giselebragailustra@gmail.com>
2025-07-11 14:41:15 -0300

    1441

--- calculadora/script.js
+++ calculadora/script.js
@@ -4,6 +4,10 @@
 //funções para realizar operações aritméticas (soma, subt, mult, divi, modu, raiz, del, zero)
 //imprimir na tela o resultado
 
-const botao_soma=document.querySelector(".+")
-const numeros = document.querySelectorAll("input")
+const botao_soma=document.querySelector("btn_soma")
+const botao_div=document.querySelector("btn_divi")
+const botao_mult=document.querySelector("btn_mult")
+const botao_subt=document.querySelector("btn_subt")
 const telaResultado = document.querySelector("result")
+
+const val = document.querySelectorAll("input")
