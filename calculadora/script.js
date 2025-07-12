6f153625b48b8c541f9757d194d74bb6a3286fbd
gi-braga <giselebragailustra@gmail.com>
2025-07-12 08:42:27 -0300

    0842

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,13 +1,35 @@
-//receber valores digitados no input
-//variavel para armazenar valor 1
-//variavel para armazenar valor 2
-//funções para realizar operações aritméticas (soma, subt, mult, divi, modu, raiz, del, zero)
-//imprimir na tela o resultado
+/*const botao_soma=document.querySelector("#btn_soma")
+const botao_div=document.querySelector("#btn_divi")
+const botao_mult=document.querySelector("#btn_mult")
+const botao_subt=document.querySelector("#btn_subt")*/
 
-const botao_soma=document.querySelector("btn_soma")
-const botao_div=document.querySelector("btn_divi")
-const botao_mult=document.querySelector("btn_mult")
-const botao_subt=document.querySelector("btn_subt")
-const telaResultado = document.querySelector("result")
+const telaResultado = document.querySelector("#result")
+const numero = document.querySelector(".num")
 
-const val = [...document.querySelectorAll("input")]
+let conta
+
+//ADICIONANDO NUMEROS NA TELA
+function botao(value) {
+    conta = telaResultado.value += value
+} 
+
+//LIMPANDO TELA (C)
+function limpar() {
+  telaResultado.value = ""
+}
+
+//APAGANDO ULTIMO NUMERO(DEL)
+function retirar() {
+    const palavraCompleta = telaResultado.value
+    const ultimaLetra = palavraCompleta.substring(0, telaResultado.value.length - 1)
+    console.log(ultimaLetra)
+    
+    telaResultado.value = ultimaLetra
+}
+
+//FUNÇÃO DE CALCULO
+function calc() {
+
+  const resultado = eval(conta)
+  telaResultado.value = resultado
+}
