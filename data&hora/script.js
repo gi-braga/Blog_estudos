f3cbb13b6ec1a73bb028b76e5b16d96a053345fc
gi-braga <giselebragailustra@gmail.com>
2025-08-06 08:15:19 -0300

    0815

---
+++ data&hora/script.js
@@ -0,0 +1,27 @@
+const data=new Date()
+
+console.log(data)
+
+/*métodos que contem na classe Date
+
+getDate() //dia do mês
+getDay() //dia da semana (0-6)
+getFullYear() //ano
+getHours() //hora
+getMilliseconds() //milissegundos
+getMinutes() //minutos
+getMonth() //mês (0-11)
+getSeconds() //segundos
+getTime() //data em milissegundos desde 1 de janeiro de 1970
+Date.now() //retorna a data atual em milissegundos desde 1 de janeiro de 1970
+getTimezoneOffset() //diferença em minutos entre UTC e o horário local
+setDate() //define o dia do mês
+setFullYear() //define o ano
+setHours() //define a hora
+setMinutes() //define os minutos
+setMonth() //define o mês
+setSeconds() //define os segundos
+setMilliseconds() //define os milissegundos
+toDateString() //retorna a data como uma string legível
+toLocaleDateString() //retorna a data formatada de acordo com as configurações locais
+*/
