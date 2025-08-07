b192ae5fc94ebd22c4e23b9e0f30881c94bec09f
gi-braga <giselebragailustra@gmail.com>
2025-08-07 05:43:46 -0300

    0543

--- b_math/script.js
+++ b_math/script.js
@@ -9,8 +9,33 @@ mat.innerHTML = Math.PI //constante Pi (relação entre a circunferência e o di
 
 mat.innerHTML = Math.abs(-5) //valor absoluto
 
-*/
-
 mat.innerHTML = Math.random() //número aleatório entre 0 e 1 (exclusivo)
 mat.innerHTML = Math.random()*10 //número aleatório entre 0 e 10 (exclusivo)
 mat.innerHTML = Math.floor(Math.random()*11) //número arredondado aleatório entre 0 e 10 (inclusivo)
+
+mat.innerHTML = Math.round(5.4) //arredonda para o inteiro mais próximo
+
+mat.innerHTML = Math.pow(2, 3) //2 elevado a 3 (2^3)
+
+mat.innerHTML = Math.sqrt(64) //raiz quadrada de 64
+*/
+
+//pegar a referencia dos elementos dos olhos
+const olhos = [...document.getElementsByClassName("olho")]
+
+let posX_mouse = 0
+let posY_mouse = 0
+
+//pegar a coordenada do mouse
+window.addEventListener("mousemove", (evt) => {
+    posX_mouse = evt.clientX //retorna um number com a posição X do mouse
+    posY_mouse = evt.clientY
+
+    const rot = Math.atan2(posY_mouse,posX_mouse) //retorna o arco tangente de dois coeficientes EM RADIANOS
+
+    olhos.forEach((olho)=>{
+        olho.style.transform = "rotate("+(rot*180/Math.PI)+"deg)" //converte radianos para graus (*180/Pi)
+    })
+
+
+})
